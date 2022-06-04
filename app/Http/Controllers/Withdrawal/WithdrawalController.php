5b9a585d3cfa901fb45e11aef9843e22fc888c29
Worlddev <worlddev5007@gmail.com>
2022-06-05 03:35:58 +0900

    Chaged withdrawal 2022.6.5

--- app/Http/Controllers/Withdrawal/WithdrawalController.php
+++ app/Http/Controllers/Withdrawal/WithdrawalController.php
@@ -24,48 +24,48 @@ class WithdrawalController extends Controller
         return view('pages.withdrawal.balance')->with(['data' => $data, 'user_data' => $user_data]);
     }
 
-    // public function withdrawal(Request $request)
-    // {
-    //     $user = Session::get('user');   // Session User Data
-    //     $userId = $user->userId;
-    //     $input_amount = $request->amount;
-    //     $wallet_address = $request->wallet_address;
-    //     $amount = round($input_amount * 1 * 0.95);
-    //     $fee = $input_amount * 1 - $amount * 1;
-    //     try {
-    //         $randomDepositId = uniqid();
+    public function withdrawal(Request $request)
+    {
+        $user = Session::get('user');   // Session User Data
+        $userId = $user->userId;
+        $input_amount = $request->amount;
+        $wallet_address = $request->wallet_address;
+        $amount = round($input_amount * 1 * 0.95);
+        $fee = $input_amount * 1 - $amount * 1;
+        try {
+            $randomDepositId = uniqid();
 
-    //         //add withdrawal fee report
-    //         $feeModel = new Withdrawal_fee();
-    //         $feeModel->userId =  $userId;
-    //         $feeModel->depositId =  $randomDepositId;
-    //         $feeModel->amount =  $input_amount;
-    //         $feeModel->fee =  $fee;
-    //         $feeModel->status =  0;
-    //         $feeModel->save();
+            //add withdrawal fee report
+            $feeModel = new Withdrawal_fee();
+            $feeModel->userId =  $userId;
+            $feeModel->depositId =  $randomDepositId;
+            $feeModel->amount =  $input_amount;
+            $feeModel->fee =  $fee;
+            $feeModel->status =  0;
+            $feeModel->save();
 
-    //         $withdrawal_balance = new Member_withdrawal_balance;
-    //         $withdrawal_balance->userId =  $userId;
-    //         $withdrawal_balance->target_w_address =  $wallet_address;
-    //         $withdrawal_balance->depositId =  $randomDepositId;
-    //         $withdrawal_balance->withdrawal_amount =  $amount;
-    //         $withdrawal_balance->input_amount =  $input_amount;
-    //         $withdrawal_balance->status =  0;
-    //         $withdrawal_balance->save();
+            $withdrawal_balance = new Member_withdrawal_balance;
+            $withdrawal_balance->userId =  $userId;
+            $withdrawal_balance->target_w_address =  $wallet_address;
+            $withdrawal_balance->depositId =  $randomDepositId;
+            $withdrawal_balance->withdrawal_amount =  $amount;
+            $withdrawal_balance->input_amount =  $input_amount;
+            $withdrawal_balance->status =  0;
+            $withdrawal_balance->save();
 
-    //         //Minus withdrawal amount from member's income_amount in member_wallet_balance table
-    //         $wallet_balance = Member_wallet_balance::where('userId', $userId)->first();
-    //         $minus_income = $wallet_balance->income_amount * 1 - $input_amount * 1;
+            //Minus withdrawal amount from member's income_amount in member_wallet_balance table
+            $wallet_balance = Member_wallet_balance::where('userId', $userId)->first();
+            $minus_income = $wallet_balance->income_amount * 1 - $input_amount * 1;
 
-    //         Member_wallet_balance::where('userId', $userId)
-    //             ->update([
-    //                 'income_amount' =>  $minus_income,
-    //             ]);
+            Member_wallet_balance::where('userId', $userId)
+                ->update([
+                    'income_amount' =>  $minus_income,
+                ]);
 
-    //         return response()->json(['status' => 200]);
-    //     } catch (\Exception $e) {
-    //         $error = 'Error: ' . $e->getMessage();
-    //         return response()->json(['status' => 400, 'error' => $error]);
-    //     }
-    // }
+            return response()->json(['status' => 200]);
+        } catch (\Exception $e) {
+            $error = 'Error: ' . $e->getMessage();
+            return response()->json(['status' => 400, 'error' => $error]);
+        }
+    }
 }
